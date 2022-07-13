ca7bed700e2ae63e773e10f4298f76bd71092286
Rubia Scutari <rubia.scutari@gmail.com>
2022-07-13 14:01:30 -0300

    Ajuste na logica da data

--- backend/src/main/java/com/devsuperior/dsmeta/services/SalesService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SalesService.java
@@ -22,8 +22,8 @@ public class SalesService {
 		
 		LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
 				
-		LocalDate min = minDate.equals("") ? today : LocalDate.parse(minDate);
-		LocalDate max = maxDate.equals("") ? today.minusDays(365) : LocalDate.parse(maxDate);
+		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
 		
 		return repository.findSales(min, max, pageable);
 	}
