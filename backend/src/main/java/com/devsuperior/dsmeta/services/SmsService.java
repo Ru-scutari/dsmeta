9bbc4e730d7ed11ee4ff20aa6f4e7bb492af0dab
Rubia Scutari <rubia.scutari@gmail.com>
2022-07-15 17:57:02 -0300

    Replace do valor de ponto para vírgula

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -36,7 +36,7 @@ public class SmsService {
 		String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 		
 		String msg = "O Vendedor " + sale.getSellerName() + " foi destaque em " + date
-				+ " com o total de R$ " + String.format("%.2f", sale.getAmount());
+				+ " com o total de R$ " + String.format("%.2f", sale.getAmount()).replace(".", ",");
 
 		Twilio.init(twilioSid, twilioKey);
 
